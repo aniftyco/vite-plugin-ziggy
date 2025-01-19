41bdb1bbb983fe4d1cfe9cb62157ac4bae9ade4f
Josh Manders <josh@joshmanders.com>
2025-01-19 14:48:50 -0600

    feat: support config options

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import { Plugin } from 'vite';
 import { run } from 'vite-plugin-run';
 
-export type Config = {};
+export type Config = {
+  path?: string;
+  only?: string[];
+  except?: string[];
+};
 
-export default (config?: Config): Plugin => {
+export default ({ path = 'node_modules/vite-plugin-ziggy/routes', only = [], except = [] }: Config = {}): Plugin => {
+  const cmd = ['php', 'artisan', 'ziggy:generate', path, '--types-only'];
+
+  if (only.length) {
+    cmd.push('--only', only.join(','));
+  }
+
+  if (except.length) {
+    cmd.push('--except', except.join(','));
+  }
+
   const { configResolved, handleHotUpdate } = run([
     {
       name: 'ziggy-generator',
-      run: ['php', 'artisan', 'ziggy:generate', 'node_modules/vite-plugin-ziggy/routes', '--types-only'],
+      run: cmd,
       condition: (file) => file.includes('/routes/') && file.endsWith('.php'),
     },
   ]);
