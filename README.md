821127b63d90a49208cec0b0c0614403dcf357b3
Josh Manders <josh@joshmanders.com>
2025-01-18 22:06:11 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ziggy Vite Plugin
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Plugin } from 'vite';
+import { run } from 'vite-plugin-run';
+
+export type Config = {};
+
+export default (config?: Config): Plugin => {
+  const { configResolved, handleHotUpdate } = run([
+    {
+      name: 'ziggy-generator',
+      run: ['php', 'artisan', 'ziggy:generate', 'node_modules/vite-plugin-ziggy/routes/index', '--types-only'],
+      condition: (file) => file.includes('/routes/') && file.endsWith('.php'),
+    },
+  ]);
+
+  return {
+    name: 'ziggy-plugin',
+    configResolved,
+    handleHotUpdate,
+  };
+};
